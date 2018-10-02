af514800c32f6ea270a0186d51fc3a3d4c0c05bb
YuumaOwen <yxy.yuuma@gmail.com>
2018-10-03 01:29:39 +0900

    logの検索コマンド用の関数追加

--- LoginCountBot/bot_module/_utility_.py
+++ LoginCountBot/bot_module/_utility_.py
@@ -15,3 +15,14 @@ class Utillity:
         f = codecs.open(path, "a", 'utf-8')
         f.write(log_message)
         f.close()
+    def searchlog(self,user_name):
+        x=0
+        path = "../log/cmd_log.txt"
+        with open(path, mode='r', newline='', encoding='utf-8') as f:
+            l = [line for line in f]
+            for a in reversed(l):
+                if a.split()[1].startswith(user_name):
+                    x+=1
+                    if x==2:
+                        return "```"+a+"```"
+            return "```ログが見つかりませんでした。```"
